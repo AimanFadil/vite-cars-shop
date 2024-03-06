4f728271382c4614a016652de9f84d05daa124d1
SebastianBuftea <sebibuftea85@gmail.com>
2024-03-06 12:13:03 +0100

    [UPODATE] scaffolding header and home

---
+++ src/pages/HomeApp.vue
@@ -0,0 +1,21 @@
+<script>
+import {store} from '../store.js';
+export default {
+    name: 'HomeApp',
+  data(){
+        return{
+            store,
+        }
+    },
+    
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="scss">
+    
+</style>
