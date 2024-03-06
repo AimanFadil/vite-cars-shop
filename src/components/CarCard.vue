9427bafec7c353966626a65c7d4cf27d16f3c513
stappost <emanuele.scarpantoni@gmail.com>
2024-03-06 16:36:54 +0100

    fix axios

--- src/components/CarCard.vue
+++ src/components/CarCard.vue
@@ -8,12 +8,12 @@ export default {
 </script>
 <template lang="">
     <div class='col-4'>
-        <router-link :to="{ name: 'single-car', params: {id: car.id} }">
+        <router-link class='decoration-none' :to="{ name: 'single-car', params: {id: car.id} }">
             <div class="card my-2" style="width: 18rem;">
                 <!-- <img class="card-img-top" :src="cars.img != null ? `${store.baseUrl}/storage/${project.logo}` : `${store.baseUrl}/placeholder/logo.jpeg`" alt="Card image cap"> -->
                 <div class="card-body">
                     <h4> {{car.modello}} </h4>
-                    <p class="card-text">{{car.descrizione }}...</p> 
+                    <p class="card-text">{{car.descrizione.substr(0, 50) }}...</p> 
                 </div>
     
             </div>
@@ -21,5 +21,7 @@ export default {
     </div>
 </template>
 <style lang="scss" scoped>
-    
+    .decoration-none{
+        text-decoration: none;
+    }
 </style>
--- src/pages/SingleCar.vue
+++ src/pages/SingleCar.vue
@@ -27,46 +27,41 @@ import axios from 'axios';
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="text-center"></h1>
-                </div>
-                <div class="col-12">
-                    <div class="image">
 
-                    </div>
+                    <h2 class="card-title m-2">{{ $car.modello }}</h2>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>PREZZO: {{ $car.prezzo }}€</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>ALIMENTAZIONE: {{ $car.alimentazione }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>ANNO: {{ $car.anno }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>DESCRIZIONE: {{ $car.descrizione }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>NUMERO TELAIO: {{ $car.numero_telaio }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>TRAZIONE: {{ $car.trazione }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>PORTE: {{ $car.porte }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>CARROZZERIA: {{ $car.carrozzeria }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>CAVALLI: {{ $car.cavalli }}</h3>
+                        </li>
+                        <li class="list-group-item bg-info-subtle border border-2 border-info">
+                            <h3>CILINDRATA: {{ $car.cilindrata }}</h3>
+                        </li>
+                    </ul>
                 </div>
-                <h5 class="card-title m-2">{{ $car.modello }}</h5>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>PREZZO: {{ $car.prezzo }}€</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>ALIMENTAZIONE: {{ $car.alimentazione }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>ANNO: {{ $car.anno }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>DESCRIZIONE: {{ $car.descrizione }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>NUMERO TELAIO: {{ $car.numero_telaio }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>TRAZIONE: {{ $car.trazione }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>PORTE: {{ $car.porte }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>CARROZZERIA: {{ $car.carrozzeria }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>CAVALLI: {{ $car.cavalli }}</h3>
-                </li>
-                <li class="list-group-item bg-info-subtle border border-2 border-info">
-                    <h3>CILINDRATA: {{ $car.cilindrata }}</h3>
-                </li>
-            </ul>
             </div>
         </div>
     </div>
