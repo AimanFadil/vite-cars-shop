49f391b61eaf53585ac43045009d6a5cdfc4a59a
SebastianBuftea <sebibuftea85@gmail.com>
2024-03-07 14:31:51 +0100

    [UPDATE] AND FIXING

--- src/pages/CarsOfHouseCarsApp.vue
+++ src/pages/CarsOfHouseCarsApp.vue
@@ -48,10 +48,32 @@ export default {
                     </div>
                 </div>
             </div> 
-        </div>
+    </div>
         <!-- se non esistono auto di quetsa categoria  -->
     <div v-if="carsOfHouse == 0">
-        <NotFoundApp/>  
+        <AppLoader v-if="store.loading"/>
+            <div v-else> 
+                <div class="container">
+                    <div class='row'>
+                        <div class="col-12 d-flex justify-content-center transparent_" @click="getCarsOfHouse()"> 
+                            <span class="badge rounded-pill hover_color m-2 p-3 " v-for="CarHouse, index in store.houseCars" :key="index">
+                            <router-link :to="{ name: 'housecars-car', params: {id: CarHouse.id} }" class="dropdown-item gold_color hover_bg"> <strong>{{CarHouse.nome}}</strong></router-link>
+                            </span>
+                        </div>
+                        <CarCard v-for="car, index in carsOfHouse" :key="index" :car="car"/>
+                    </div>
+                </div>
+            </div> 
+            <div class="container ">
+                <div class="row d-flex justify-content-center ">
+                    <div class="col-5 color_errore rounded text-center align-items-center mt-5 ">
+                       <h1 class="text-white">
+                             <i class="fa-solid fa-circle-exclamation" style="color: #ffffff;"></i>
+                             Non ci sono risultati da mostrare :( 
+                        </h1>
+                    </div>
+                </div>
+            </div>
     </div>
 </template>
 <style lang="scss" scoped>
@@ -66,5 +88,9 @@ background-color: rgba(0, 0, 0, 0.32);
     &:hover{
             color: rgb(173, 134, 71);
     }
+}
+
+.color_errore{
+    background-color: rgba(255, 0, 0, 0.322);
 }
 </style>
